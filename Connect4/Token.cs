15c511ea7ef3c943644fe22f2b0718b9d903cafe
Koenraad Pecceu <koenraad.pecceu@creo.be>
2024-05-31 09:48:57 +0200

    Add token class

---
+++ Connect4/Token.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Connect4
+{
+    // overerven van Panel om achtergrondkleur, grootte, ... te kunnen instellen
+    internal class Token : Panel
+    {
+        // constructor met basis waarden voor een token
+        public Token()
+        {
+            // grootte 50 op 50 pixels
+            Height = 50;
+            Width = Height;
+            // achtergrondkleur standaard op rood zetten
+            BackColor = Color.Red;
+        }
+    }
+}
--- Connect4/frmConnect4.cs
+++ Connect4/frmConnect4.cs
@@ -10,9 +10,9 @@ using System.Windows.Forms;
 
 namespace Connect4
 {
-    public partial class Form1 : Form
+    public partial class frmConnect4 : Form
     {
-        public Form1()
+        public frmConnect4()
         {
             InitializeComponent();
         }
