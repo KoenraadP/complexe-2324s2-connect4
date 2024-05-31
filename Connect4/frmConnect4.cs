2e1ce50d4552ccf50e2a4cad7b3cf42aa7ac18a9
Koenraad Pecceu <koenraad.pecceu@creo.be>
2024-05-31 10:33:56 +0200

    Add buttons

--- Connect4/frmConnect4.cs
+++ Connect4/frmConnect4.cs
@@ -41,21 +41,52 @@ namespace Connect4
                 // tweede loop die alle kolom indexen overloopt
                 for (int col = 0;  col < grid.GetLength(1); col++)
                 {
+                    // eerste iteratie loop --> row 0, col 0
+                    // tweede iteratie loop --> row 0, col 1
+
                     // token aanmaken
                     Token t = new Token();
                     // standaard --> grijze tokens (vakjes)
                     t.BackColor = Color.LightGray;
+                    // locatie van token instellen
+                    t.Location = new Point(col * 60 + 50, row * 60 + 50);
                     // token op form plaatsen
                     Controls.Add(t);
                 }
             }
         }
 
+        // methode om knoppen aan te maken bovenaan
+        private void GenerateButtons()
+        {
+            // voor iedere kolom een button maken
+            for (int col = 0; col < grid.GetLength(1); col++)
+            {
+                // button aanmaken en grootte instellen
+                Button btn = new Button();
+                btn.Size = new Size(50, 25);
+                // naam geven aan button
+                btn.Name = "btnCol" + col; // eerste knop is dan btnCol0
+                // locatie --> telkens opschuiven naar rechts
+                btn.Location = new Point(col * 60 + 50, 10);
+                // op form plaatsen
+                Controls.Add(btn);
+            }
+
+            // button aanmaken
+            // Button b = new Button();
+            // button op form plaatsen
+            // Controls.Add(b);
+        }
+
         // Load event --> wordt automatisch uitgevoerd
         // nadat form volledig ingeladen is
         private void frmConnect4_Load(object sender, EventArgs e)
         {
+            // toon alle tokens
             ShowTokens();
+            // toon alle buttons
+            GenerateButtons();
 
             // nieuwe token maken
             // Token t = new Token();
