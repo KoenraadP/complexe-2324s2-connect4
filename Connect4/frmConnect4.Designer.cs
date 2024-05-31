dda8ba2cc52768013176bfb5292a72485bf37f23
Koenraad Pecceu <koenraad.pecceu@creo.be>
2024-05-31 10:17:40 +0200

    Add beginning of showtokens

--- Connect4/frmConnect4.Designer.cs
+++ Connect4/frmConnect4.Designer.cs
@@ -37,6 +37,7 @@
             this.ClientSize = new System.Drawing.Size(504, 411);
             this.Name = "frmConnect4";
             this.Text = "Form1";
+            this.Load += new System.EventHandler(this.frmConnect4_Load);
             this.ResumeLayout(false);
 
         }
--- Connect4/frmConnect4.cs
+++ Connect4/frmConnect4.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.Diagnostics;
 using System.Drawing;
 using System.Linq;
 using System.Text;
@@ -10,11 +11,64 @@ using System.Windows.Forms;
 
 namespace Connect4
 {
+    // ieder veld moet de waarde
+    // empty (leeg), red of yellow krijgen
+    enum State
+    {
+        Empty,
+        Red,
+        Yellow
+    }
+
     public partial class frmConnect4 : Form
     {
+        // globale array maken met states
+        // dit is ons code speelveld
+        // 6 rijen, 7 kolommen
+        State[,] grid = new State[6, 7];
+
         public frmConnect4()
         {
             InitializeComponent();
+        }
+
+        // methode om alle tokens te tonen op de juiste plaats
+        private void ShowTokens()
+        {
+            // loop die alle rij indexen overloopt
+            for (int row = 0; row < grid.GetLength(0); row++)
+            {
+                // tweede loop die alle kolom indexen overloopt
+                for (int col = 0;  col < grid.GetLength(1); col++)
+                {
+                    // token aanmaken
+                    Token t = new Token();
+                    // standaard --> grijze tokens (vakjes)
+                    t.BackColor = Color.LightGray;
+                    // token op form plaatsen
+                    Controls.Add(t);
+                }
+            }
+        }
+
+        // Load event --> wordt automatisch uitgevoerd
+        // nadat form volledig ingeladen is
+        private void frmConnect4_Load(object sender, EventArgs e)
+        {
+            ShowTokens();
+
+            // nieuwe token maken
+            // Token t = new Token();
+            // locatie voor token bepalen
+            // ClientSize.Width = breedte van het veld
+            // ClientSize.Height = hoogte van het veld
+            // t.Location = new Point(ClientSize.Width - t.Width, ClientSize.Height - t.Height);
+            // token op form plaatsen
+            // Controls.Add(t);
+
+            // controleren wat er momenteel op de eerste plaats
+            // van ons grid staat
+            // Debug.WriteLine(grid[0, 0]);
         }
     }
 }
